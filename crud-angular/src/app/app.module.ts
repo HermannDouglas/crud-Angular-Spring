256990c8fdd5143da0673d67968179b86756c42c
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-05-29 13:24:16 -0500

    Feat: Integrar toolbar do Angular Material e cria módulo Courses
    - Adicionado o MatToolbarModule ao módulo da aplicação (AppModule) e incluída uma toolbar no componente principal da aplicação (AppComponent).
    - Criado um novo módulo `Courses` com roteamento e um componente `Courses` básico.
    - Implementado um template simples para o componente `Courses`.
    - Adicionados estilos e especificações de teste para o componente `Courses`.
    - Estabelecido um tema personalizado para o Angular Material no arquivo `styles.scss`.

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
+  providers: [provideClientHydration(withEventReplay())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
