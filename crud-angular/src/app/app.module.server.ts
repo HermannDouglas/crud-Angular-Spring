e2e2c2439f8a86d46c7f53ccc813335f913b1f81
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-02 17:16:14 -0500

    Refactor: Atualizar configuração do servidor para utilizar provideServerRendering
    - Remove a importação do ServerModule e ajusta a configuração do AppModule para usar a nova função provideServerRendering com as rotas do servidor.

--- crud-angular/src/app/app.module.server.ts
+++ crud-angular/src/app/app.module.server.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
-import { withRoutes } from '@angular/ssr';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { serverRoutes } from './app.routes.server';
 
 @NgModule({
-  imports: [AppModule, ServerModule],
+  imports: [AppModule],
   providers: [provideServerRendering(withRoutes(serverRoutes))],
   bootstrap: [AppComponent],
 })
