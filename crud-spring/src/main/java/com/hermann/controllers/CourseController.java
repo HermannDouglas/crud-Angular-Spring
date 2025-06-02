9fc10c3884a1676dccd8f63a67844b66d15f7fb9
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-02 11:30:45 -0500

    Feat: Adicionar entidade Course
    - Adiciona model, repository e controller de Course.
    - Remove `HelloController`.

---
+++ crud-spring/src/main/java/com/hermann/controllers/CourseController.java
@@ -0,0 +1,42 @@
+package com.hermann.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hermann.models.Course;
+import com.hermann.repositories.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    // @Autowired
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(CourseRepository courseRepository) {
+        return args -> {
+            courseRepository.deleteAll();
+
+            Course course = new Course();
+            course.setName("Angular com Spring");
+            course.setCategory("Frontend");
+
+            courseRepository.save(course);
+        };
+    }
+}
