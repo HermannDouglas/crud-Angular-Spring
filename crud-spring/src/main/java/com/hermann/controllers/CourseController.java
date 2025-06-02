d3259de91224c1ae616caadb3ebc2273d9b38e99
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-02 16:19:26 -0500

    Feat(frontend): Atualizar dependências do Angular e refatorar configuração do projeto
    - Atualiza as dependências do Angular para a versão 20.0.0 no package.json e package-lock.json.
    - Modifica a configuração do builder no angular.json para utilizar o novo pacote @angular/build.
    - Refatora a configuração do servidor no app.module.server.ts para usar a nova função provideServerRendering.
    Backend:
    - Remove importações desnecessárias no CourseController.java.

--- crud-spring/src/main/java/com/hermann/controllers/CourseController.java
+++ crud-spring/src/main/java/com/hermann/controllers/CourseController.java
@@ -2,7 +2,6 @@ package com.hermann.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
