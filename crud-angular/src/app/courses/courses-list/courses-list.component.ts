f2927b62bab95ab110901207d7e285d768788e45
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-05 16:59:33 -0500

    Feat(Frontend): Adicionar componente CoursesList e refatorar exibição de cursos
    - Adiciona o componente CoursesList para encapsular a lógica de exibição da lista de cursos.
    - Refatora o template do CoursesComponent para utilizar o novo componente, melhorando a organização do código.
    - Atualiza estilos do mat-card e implementa layout flexível para melhor apresentação dos cursos.

---
+++ crud-angular/src/app/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-courses-list',
+  standalone: false,
+  templateUrl: './courses-list.component.html',
+  styleUrl: './courses-list.component.scss',
+})
+export class CoursesListComponent implements OnInit {
+
+  @Input()
+  courses: Course[] = [];
+
+  readonly displayedColumns = ['name', 'category', 'actions'];
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {}
+
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route });
+  }
+}
