f175282e29bf63fe3aa63b9473645a64a51ab96c
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-06 10:21:45 -0500

    Feat(Frontend): Adicionar funcionalidade de edição de cursos e resolver dados na rota
    - Atualiza o sistema de rotas para incluir a resolução de dados com CourseResolver nas rotas de criação e edição de cursos.
    - Implementa a lógica de edição no CoursesListComponent, emitindo o curso selecionado para o CoursesComponent.
    - Adiciona método onEdit no CoursesComponent para navegar à página de edição do curso.
    - Modifica o CoursesService para incluir método loadById para buscar curso por ID.

--- crud-spring/src/main/java/com/hermann/controllers/CourseController.java
+++ crud-spring/src/main/java/com/hermann/controllers/CourseController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.hermann.models.Course;
 import com.hermann.repositories.CourseRepository;
