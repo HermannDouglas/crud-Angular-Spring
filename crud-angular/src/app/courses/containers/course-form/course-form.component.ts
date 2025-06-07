e7737f14deb392c840707fae060e7c98956b0525
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-07 13:24:07 -0500

    Feat(Frontend): Melhorar formulário de cursos com validações e feedback
    - Adiciona validações de campo obrigatório e limites de caracteres para o nome e categoria no formulário de cursos.
    - Implementa mensagens de erro personalizadas para feedback ao usuário.
    - Ajusta o layout do formulário para melhor apresentação e usabilidade.

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { FormGroup, NonNullableFormBuilder } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
@@ -28,8 +28,15 @@ export class CourseFormComponent implements OnInit {
 
     this.form = this.formBuilder.group({
       _id: [course ? course._id : ''],
-      name: [course ? course.name : ''],
-      category: [course ? course.category : ''],
+      name: [
+        course ? course.name : '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(150),
+        ],
+      ],
+      category: [course ? course.category : '', [Validators.required]],
     });
   }
 
@@ -51,5 +58,29 @@ export class CourseFormComponent implements OnInit {
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso!', '', { duration: 5000 });
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório.';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors
+        ? field.errors['minlength']['requiredLength']
+        : 3;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors
+        ? field.errors['maxlength']['requiredLength']
+        : 150;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo invalido.';
   }
 }
