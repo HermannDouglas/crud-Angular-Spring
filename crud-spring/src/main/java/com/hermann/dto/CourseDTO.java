627d61f37472345758d53b186357bc5f3b41c2e6
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-09 19:47:04 -0500

    feat: Adiciona CourseDTO para representar e validar dados de curso

---
+++ crud-spring/src/main/java/com/hermann/dto/CourseDTO.java
@@ -0,0 +1,16 @@
+package com.hermann.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CourseDTO(
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 3, max = 150) String name,
+        @NotNull @Length(max = 100) @Pattern(regexp = "Backend|Frontend") String category) {
+
+}
