c1c709c28119823942d34e1f4cd9e5e12df32ce3
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-11 18:54:15 -0500

    Refactor(Backend): Atualizar CourseDTO e CourseMapper para usar LessonDTO
    - Altera o tipo da lista de aulas no CourseDTO de 'Lesson' para 'LessonDTO', melhorando a estrutura de dados.
    - Atualiza o CourseMapper para mapear corretamente as aulas do modelo Course para o novo formato LessonDTO, garantindo a consistência na representação dos dados.

--- crud-spring/src/main/java/com/hermann/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/hermann/dto/CourseDTO.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.hermann.model.Lesson;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,6 +14,6 @@ public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotBlank @NotNull @Length(min = 3, max = 150) String name,
         @NotNull @Length(max = 100) @Pattern(regexp = "Backend|Frontend") String category,
-        List<Lesson> lessons) {
+        List<LessonDTO> lessons) {
 
 }
