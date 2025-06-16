37be49e48646b33447e5c77d7dc328b198d97fc6
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-06-16 14:29:01 -0500

    Refactor(Frontend): Integrar FormUtilsService para gerenciamento de erros no CourseForm
    - Substitui chamadas diretas de validação por métodos do FormUtilsService, centralizando a lógica de tratamento de erros.
    - Atualiza mensagens de erro para campos de nome e URL das aulas, melhorando a clareza das validações.
    - Refatora a verificação de obrigatoriedade do FormArray de aulas, utilizando o FormUtilsService para uma abordagem mais consistente.

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -11,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
 import { Lesson } from '../../model/lesson';
+import { FormUtilsService } from '../../../shared/form/form-utils.service';
 
 @Component({
   selector: 'app-course-form',
@@ -26,7 +27,8 @@ export class CourseFormComponent implements OnInit {
     private service: CoursesService,
     private snackBar: MatSnackBar,
     private location: Location,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public formUtils: FormUtilsService
   ) {}
 
   ngOnInit(): void {
@@ -48,8 +50,6 @@ export class CourseFormComponent implements OnInit {
         Validators.required
       ),
     });
-    console.log(this.form);
-    console.log(this.form.value);
   }
 
   private retrieveLessons(course: Course) {
@@ -109,7 +109,7 @@ export class CourseFormComponent implements OnInit {
         (error) => this.onError()
       );
     } else {
-      alert('Formulário inválido!');
+      this.formUtils.validadeAllFormFields(this.form);
     }
   }
 
@@ -124,34 +124,5 @@ export class CourseFormComponent implements OnInit {
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso!', '', { duration: 5000 });
-  }
-
-  getErrorMessage(fieldName: string) {
-    const field = this.form.get(fieldName);
-
-    if (field?.hasError('required')) {
-      return 'Campo obrigatório.';
-    }
-
-    if (field?.hasError('minlength')) {
-      const requiredLength = field.errors
-        ? field.errors['minlength']['requiredLength']
-        : 3;
-      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
-    }
-
-    if (field?.hasError('maxlength')) {
-      const requiredLength = field.errors
-        ? field.errors['maxlength']['requiredLength']
-        : 150;
-      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
-    }
-
-    return 'Campo invalido.';
-  }
-
-  isFormArrayRequired() {
-    const lessons = this.form.get('lessons') as UntypedFormArray;
-    return !lessons.valid && lessons.hasError('required') && lessons.touched;
   }
 }
